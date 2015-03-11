d6fcbe470de923259ac1efe45a7d70b4b657a165
Anne van Rossum <anne@dobots.nl>
2015-03-11 12:16:23 +0100

    Show multiple plots at the same time. Play around with fft

--- scripts/show.m
+++ scripts/show.m
@@ -1,13 +1,25 @@
-device='fridge';
-device='bulb';
-device='bulb1';
+
+devices={"fridge", "bulb"};
+
+% loop over everything multiple times
+for iter = 1:length(devices)
+
+device=devices{iter};
+
 file=['../data/' device '.txt.filtered'];
 c=load('-ascii',file);
 ofile=[device '.png'];
 
-% get time stamps and make them a bit smaller
+% configuration options
+smooth_raw_flag=true;
+smooth_avg_flag=true;
+remove_above_threshold_flag=true;
+make_log_flag=false;
+subsample_flag=false;
+apply_fft_flag=true;
+
+% get time stamps and make them a bit smaller (by subtracting first time stamp)
 d=c(:,1)-c(1,1);
-%d=c(:,1);
 
 % clock times are in reverse order
 tbeg=c(:,end);
@@ -22,13 +34,19 @@ data_end=1;
 %e=c(:,2:end); if there are no clock times involved
 e=c(:,data_start:end-data_end);
 
+% data error, remove stuff above threshold
+if (remove_above_threshold_flag)
+	threshold=80;
+	e(e>threshold)=0;
+end
+
+% cumulative vector (current average, but without scaling)
+c1=sum(e')'/length(e);
+
 % prepend every series of value with 0, as well as append it, so if they are far apart it won't generate
 % weird lines
 z=zeros(size(e,1),1);
 e=[z e z];
-
-% cumulative vector (current average, but without scaling)
-c1=sum(c(:,data_start:end-data_end)')'/length(e);
 
 % we want to pair d(1,1) with e(1,:)
 % the difference between d(n,1) and d(n+1,1) is something around 20 seconds
@@ -65,30 +83,60 @@ filter_end=length(x);
 x=x(filter_start:filter_end);
 y=y(filter_start:filter_end);
 
-% data error, remove stuff above threshold
-%threshold=100;
-%y(y>threshold)=0;
+if (smooth_raw_flag)
+	wndw=1000;
+	printf("Smooth raw data with window size %i\n", wndw);
+	y = imfilter(y, fspecial('average', [wndw 1]));
+end
+
+if (smooth_avg_flag)
+	wndw=10;
+	printf("Smooth averaged data with window size %i\n", wndw);
+	c1 = imfilter(c1, fspecial('average', [wndw 1]));
+end
 
 % show log
-%y=log(y+1);
+if (make_log_flag) 
+	y=log(y+1);
+end
 
 % subsample
-%sample_freq=100;
-%x=x(1:sample_freq:end);
-%y=y(1:sample_freq:end);
+if (subsample_flag)
+	sample_freq=100;
+	x=x(1:sample_freq:end);
+	y=y(1:sample_freq:end);
+end
 
-%figure(1);
-subplot(1,2,1);
+% as
+figure(iter);
+
+subplot(2,2,1);
 %clf();
 plot(x,y);
 
 %axis ("square");
-subplot(1,2,2);
+subplot(2,2,2);
 
 plot(d,c1,'.');
-p=ylim
+p=ylim;
 set(gca, "ylim", [0 p(2) * 1.1]);
 %axis ("square");
 
+subplot(2,2,3);
+periodogram(y);
+
+subplot(2,2,4);
+if (apply_fft_flag) 
+	fc1=fft(y);
+	fakey=1:length(fc1);
+	plot(fakey,abs(fc1));
+end
+
+
 % try to save to file
 %saveas(1,ofile);
+
+end
+
+
+
