3e7b5d51fc98cf38ca2f7ccb4476a832a09e6183
Vliedel <bart@dobots.nl>
2015-04-16 17:21:17 +0200

    added reading of temperature

---
+++ getTemperature.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+__author__ = 'Bart van Vliet'
+
+
+import os, sys, datetime
+from bleAutomator import *
+
+
+
+charac='f5f90001-59f9-11e4-aa15-123b93f75cba'
+
+if __name__ == '__main__':
+	try:
+		parser = optparse.OptionParser(usage='%prog [-v] [-i <interface>] -a <dfu_target_address>\n\nExample:\n\tdfu.py -i hci0 -a cd:e3:4a:47:1c:e4',
+									version='0.1')
+		
+		parser.add_option('-a', '--address',
+				action='store',
+				dest="address",
+				type="string",
+				default=None,
+				help='DFU target address. (Can be found by running "hcitool lescan")'
+				)
+		parser.add_option('-i', '--interface',
+				action='store',
+				dest="interface",
+				type="string",
+				default="hci0",
+				help='HCI interface to be used.'
+				)
+		parser.add_option('-v', '--verbose',
+				action='store_true',
+				dest="verbose",
+				help='Be verbose.'
+				)
+		
+		options, args = parser.parse_args()
+	
+	except Exception, e:
+		print e
+		print "For help use --help"
+		sys.exit(2)
+	
+	if (not options.address):
+		parser.print_help()
+		exit(2)
+	
+	ble_rec = BleAutomator(options.interface, options.verbose)
+	
+	# Connect to peer device.
+	if (not ble_rec.connect(options.address)):
+		exit(1)
+	
+	# Make the crownstone sample the current, give it some time to sample
+	readStr = ble_rec.readString(charac)
+	if (readStr == False):
+		print "Couldn't read temperature"
+		exit(1)
+	arr8 = convert_hex_string_to_uint8_array(readStr)
+	temperature = convert_uint8_to_uint32(arr8)
+	print "Temperature: %iC" % (temperature)
+	
+	time.sleep(1)
+	
+	# Disconnect from peer device if not done already and clean up.
+	ble_rec.disconnect()
+	
+	exit(0)
