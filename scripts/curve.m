25f162c803425ba5244837420aac6816ec615929
Vliedel <bart@dobots.nl>
2015-08-26 10:32:48 +0200

    more options for curve.m

--- scripts/curve.m
+++ scripts/curve.m
@@ -1,14 +1,29 @@
 % Load data
-device='halogen'
+%device='bulb'
+%device='fridge'
+device='tv'
+%device='halogen'
 
+flag_timestamped=false
 flag_hold_on=false
+flag_reverse_time=false
 
 file=['../data/' device '.txt.filtered2']
 data=load('-ascii', file);
 datetimes  = data(:,1) - data(1,1);
-indsplit = (size(data,2)-1)/2+1;
-current    = data(:,2:indsplit);
-timestamps = (data(:,indsplit+1:end) - data(:,indsplit+1)) / 32768;
+if (flag_timestamped)
+	indsplit = (size(data,2)-1)/2+1;
+	current = data(:,2:indsplit);
+	timestamps = (data(:,indsplit+1:end) - data(:,indsplit+1)) / 32768;
+else
+	current = data(:,3:end-1);
+	if (flag_reverse_time)
+		timestamps = linspace(0, data(:,2)-data(:,end), size(data,2)-3);
+	else
+		timestamps = linspace(0, data(:,end)-data(:,2), size(data,2)-3);
+	end
+end
+
 
 % Find curves where the device is on
 [i,j]=find((max(current,[],2)>10 & max(current,[],2)<500));
