987e1ff1f0c4aba98307edfc690a1190fea8cb2d
Vliedel <bart@dobots.nl>
2016-01-12 17:01:38 +0100

    This was written by Anne

--- getPowerMateButton.py
+++ getPowerMateButton.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-__author__ = 'Bart van Vliet'
+__author__ = 'Anne van Rossum'
 
 
 import os, sys, datetime
@@ -11,30 +11,30 @@ charac='9cf53570-ddd9-47f3-ba63-09acefc60415'
 
 if __name__ == '__main__':
 	try:
-            parser = optparse.OptionParser(usage='%prog [-v] [-i <interface>] -a <ble address>\n\nExample:\n\t%prog -i hci0 -a 00:12:92:08:05:16',
-                    version='0.1')
+		parser = optparse.OptionParser(usage='%prog [-v] [-i <interface>] -a <ble address>\n\nExample:\n\t%prog -i hci0 -a 00:12:92:08:05:16',
+				version='0.1')
 
-            parser.add_option('-a', '--address',
-                    action='store',
-                    dest="address",
-                    type="string",
-                    default=None,
-                    help='DFU target address. (Can be found by running "hcitool lescan")'
-                    )
-            parser.add_option('-i', '--interface',
-                    action='store',
-                    dest="interface",
-                    type="string",
-                    default="hci0",
-                    help='HCI interface to be used.'
-                    )
-            parser.add_option('-v', '--verbose',
-                    action='store_true',
-                    dest="verbose",
-                    help='Be verbose.'
-                    )
+		parser.add_option('-a', '--address',
+				action='store',
+				dest="address",
+				type="string",
+				default=None,
+				help='DFU target address. (Can be found by running "hcitool lescan")'
+				)
+		parser.add_option('-i', '--interface',
+				action='store',
+				dest="interface",
+				type="string",
+				default="hci0",
+				help='HCI interface to be used.'
+				)
+		parser.add_option('-v', '--verbose',
+				action='store_true',
+				dest="verbose",
+				help='Be verbose.'
+				)
 
-            options, args = parser.parse_args()
+		options, args = parser.parse_args()
 
 	except Exception, e:
 		print e
@@ -51,18 +51,18 @@ if __name__ == '__main__':
 	if (not ble_rec.connect(options.address, "")):
 		exit(1)
 	
-   # Make the crownstone sample the current, give it some time to sample
+	# Make the crownstone sample the current, give it some time to sample
 	readStr = ble_rec.readStringsFirst(charac)
-		if (readStr == False):
-			print "Couldn't read button state"
-			exit(1)
+	if (readStr == False):
+		print "Couldn't read button state"
+		exit(1)
 
-		while True:
-			button_state = readStr
-			print "Button state: %s" % (button_state)
-			readStr = ble_rec.readStringsNext(charac)
+	while True:
+		button_state = readStr
+		print "Button state: %s" % (button_state)
+		readStr = ble_rec.readStringsNext(charac)
 
-   # Disconnect from peer device if not done already and clean up.
+	# Disconnect from peer device if not done already and clean up.
 	ble_rec.disconnect()
 	
 	exit(0)
