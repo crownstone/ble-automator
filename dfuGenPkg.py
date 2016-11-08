74fc1d3fd569cef712b0c15879ae952f9262b3ac
Dominik Egger <dominik@dobots.nl>
2016-11-08 17:48:07 +0100

    added combined dfu zip

--- dfuGenPkg.py
+++ dfuGenPkg.py
@@ -18,38 +18,39 @@ import copy
 from os.path import basename
 
 manifestTemplate = {
-    "manifest": {
-        "dfu_version": 0.8
-    }
+	"manifest": {
+		"dfu_version": 0.8
+	}
 }
 
 deviceTemplate = {
-    "bin_file": "",
-    "dat_file": "",
-    "init_packet_data": {
-        "application_version": 4294967295,
-        "device_revision": 65535,
-        "device_type": 65535,
-        "firmware_crc16": 0,
-        "softdevice_req": []
-    }
+	"bin_file": "",
+	"dat_file": "",
+	"init_packet_data": {
+		"application_version": 4294967295,
+		"device_revision": 65535,
+		"device_type": 65535,
+		"firmware_crc16": 0,
+		"softdevice_req": []
+	}
 }
 
 def hexFileToArr(filename):
+	print "filename", filename
 	ih = IntelHex(filename)
 	buf = ih.tobinarray()
-        log.d("End and start of the binary: " + str(ih._get_start_end()))
+	log.d("End and start of the binary: " + str(ih._get_start_end()))
 	return buf
 
 def crcFromHex(filename):
 	buf = hexFileToArr(filename)
-        len_buf = len(buf)
-        barename = os.path.splitext(basename(filename))[0]
-        log.d("Total length: " + str(len_buf) + " (check with size " + barename + ".bin on the commandline and resulting zip file)")
+	len_buf = len(buf)
+	barename = os.path.splitext(basename(filename))[0]
+	log.d("Total length: " + str(len_buf) + " (check with size " + barename + ".bin on the commandline and resulting zip file)")
 	crc = Bluenet.crc16_ccitt(buf, len_buf)
-        crc_hex_st = hex(crc)
-        crc_int_st = str(int(crc_hex_st, 16))
-        log.d("Calculated crc16_ccitt: " + crc_hex_st + "(hex) or " + crc_int_st + " (dec)")
+	crc_hex_st = hex(crc)
+	crc_int_st = str(int(crc_hex_st, 16))
+	log.d("Calculated crc16_ccitt: " + crc_hex_st + "(hex) or " + crc_int_st + " (dec)")
 	return crc
 
 def verifyFile(filename):
@@ -64,67 +65,90 @@ def verifyFile(filename):
 		exit(2)
 
 def createDatFile(name, crc, sd_req):
-        log.i("Create .dat file")
+	log.i("Create .dat file")
 	buf = struct.pack('<IHHHHH', 0xffffffff, 0xffff, 0xffff, 1, int(sd_req, 16), crc)
 	file = open('tmp/%s.dat' %name, 'wb')
 	file.write(buf)
 	file.close()
 
-def createBinFile(name):
-#        bname = basename(name);
-	buf = hexFileToArr(name + '.hex')
-        file = open('tmp/%s.bin' %name, 'wb')
-        file.write(buf)
-        file.close()
+def createBinFile(filename, name):
+	#        bname = basename(name);
+	buf = hexFileToArr(filename)
+	file = open('tmp/%s.bin' %name, 'wb')
+	file.write(buf)
+	file.close()
 
-def createApplicationDFU(filename, sd_req):
-        log.i("Create application dfu file")
-	createDFU("application", filename, sd_req)
-
-def createDFU(type, filename, sd_req):
-	verifyFile(filename)
-
-	path, nameExt = os.path.split(filename)
-	name, ext = os.path.splitext(nameExt)
-
-        log.i("Create tmp directory")
-	os.mkdir('tmp')
-	try:
-		# os.chdir('tmp')
-		crc = crcFromHex(filename)
-		createDatFile(name, crc, sd_req)
-
-		createManifest(type, name, crc, sd_req)
-
-                createBinFile(name)
-		#shutil.copyfile(filename, 'tmp/' + nameExt)
-
-		# os.chdir('..')
-                log.i("Create " + type + ".zip file") 
-		shutil.make_archive(type, 'zip', 'tmp')
-	finally:
-                log.i("Remove tmp directory, recursively")
-		shutil.rmtree('tmp')
-
-def createManifest(type, name, crc, sd_req):
-        log.i("Create manifest")
-	manifest = copy.deepcopy(manifestTemplate)
+def createManifest(type, name, crc, sd_req, manifest = None):
+	if not manifest:
+		log.i("Create manifest")
+		manifest = copy.deepcopy(manifestTemplate)
+	else:
+		log.i("Append manifest")
 	device = copy.deepcopy(deviceTemplate)
 	device["bin_file"] = '%s.bin' %name
 	device["dat_file"] = '%s.dat' %name
 	device["init_packet_data"]["firmware_crc16"] = crc
 	device["init_packet_data"]["softdevice_req"].append(int(sd_req, 16))
 	manifest["manifest"][type] = device
+	return manifest
 
+def writeManifest(manifest):
 	file = open('tmp/manifest.json', 'w')
 	json.dump(manifest, file, indent = 4, sort_keys = True)
 	file.close()
 
+def createDFU(type, filename, sd_req, manifest = None):
+	verifyFile(filename)
+
+	path, nameExt = os.path.split(filename)
+	name, ext = os.path.splitext(nameExt)
+
+	# os.chdir('tmp')
+	crc = crcFromHex(filename)
+	createDatFile(name, crc, sd_req)
+	createBinFile(filename, name)
+
+	return createManifest(type, name, crc, sd_req, manifest)
+
+def prepare():
+	log.i("Create tmp directory")
+	os.mkdir('tmp')
+
+def writeToZip(path, type):
+	try:
+		log.i("Create " + type + ".zip file")
+		shutil.make_archive(path + '/' + type, 'zip', base_dir='.', root_dir='tmp')
+		# shutil.make_archive(type, 'zip', 'tmp')
+	finally:
+		log.i("Remove tmp directory, recursively")
+		shutil.rmtree('tmp')
+
+
+def createApplicationDFU(filename, sd_req):
+	log.i("Create application dfu file")
+	prepare()
+	manifest = createDFU("application", filename, sd_req)
+	writeManifest(manifest);
+	writeToZip(os.path.dirname(filename), "application");
+
 def createBootloaderDFU(filename, sd_req):
-	createDFU("bootloader", filename, sd_req)
+	prepare()
+	manifest = createDFU("bootloader", filename, sd_req)
+	writeManifest(manifest);
+	writeToZip(os.path.dirname(filename), "bootloader");
 
 def createSoftdeviceDFU(filename, sd_req):
-	createDFU("softdevice", filename, sd_req)
+	prepare()
+	manifest = createDFU("softdevice", filename, sd_req)
+	writeManifest(manifest);
+	writeToZip(os.path.dirname(filename), "softdevice");
+
+def createCombinedBlAppDFU(bootloaderFilename, applicationFilename, sd_req):
+	prepare()
+	manifest = createDFU("bootloader", bootloaderFilename, sd_req)
+	manifest = createDFU("application", applicationFilename, sd_req, manifest)
+	writeManifest(manifest);
+	writeToZip(os.path.dirname(bootloaderFilename), "combined_bl_app");
 
 if __name__ == '__main__':
 
@@ -155,6 +179,11 @@ if __name__ == '__main__':
 				type="string",
 				default=None,
 				help='Softdevice hex file'
+		)
+		parser.add_option('-c', '--combined',
+				action='store_true',
+				dest="combined",
+				help='Create combined zip'
 		)
 		parser.add_option('-r', '--sd-req',
 				action='store',
@@ -193,15 +222,19 @@ if __name__ == '__main__':
 		exit(2)
 
 	if (options.verbose):
-		log.setLogLevel(logging.LogLevel.DEBUG)
+		log.setLevel(logging.D)
 	if (options.debug):
-		log.setLogLevel(logging.LogLevel.DEBUG2)
+		log.setLevel(logging.D2)
 
-	if options.application_hex:
-		createApplicationDFU(options.application_hex, options.sd_req)
+	if options.combined:
+		if options.application_hex and options.bootloader_hex:
+			createCombinedBlAppDFU(options.bootloader_hex, options.application_hex, options.sd_req)
+	else:
+		if options.application_hex:
+			createApplicationDFU(options.application_hex, options.sd_req)
 
-	if options.bootloader_hex:
-		createBootloaderDFU(options.bootloader_hex, options.sd_req)
+		if options.bootloader_hex:
+			createBootloaderDFU(options.bootloader_hex, options.sd_req)
 
-	if options.softdevice_hex:
-		createSoftdeviceDFU(options.softdevice_hex, options.sd_req)
+		if options.softdevice_hex:
+			createSoftdeviceDFU(options.softdevice_hex, options.sd_req)
